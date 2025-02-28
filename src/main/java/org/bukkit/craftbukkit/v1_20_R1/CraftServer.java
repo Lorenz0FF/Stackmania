5b5cb303385a798b553f59a693d186933eb1cf02
Mgazul <mgazul@163.com>
2025-02-28 22:33:00 +0800

    Fix some bug

--- src/main/java/org/bukkit/craftbukkit/v1_20_R1/CraftServer.java
+++ src/main/java/org/bukkit/craftbukkit/v1_20_R1/CraftServer.java
@@ -1116,7 +1116,7 @@ public final class CraftServer implements Server {
         }
 
         ServerLevel internal = new ServerLevel(console, console.executor, worldSession, worlddata, worldKey, worlddimension, getServer().progressListenerFactory.create(11),
-                worlddata.isDebugWorld(), j, creator.environment() == Environment.NORMAL ? list : ImmutableList.of(), true, console.overworld().getRandomSequences(), creator.environment(), generator, biomeProvider);
+                worlddata.isDebugWorld(), j, creator.environment() == Environment.NORMAL ? list : ImmutableList.of(), true, console.overworld().getRandomSequences()).craftWorldData(creator.environment(), generator, biomeProvider);
 
         name = name.contains("DIM") ? name : name.toLowerCase(java.util.Locale.ENGLISH);
         if (!(worlds.containsKey(name))) {
